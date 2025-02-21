b92b4321cc240e1d180e181a285acb01ebd7d886
Chris Deely <cdeely@musicchoice.com>
2025-02-21 13:09:49 -0500

    flag for absolute segment uris

--- src/cli.js
+++ src/cli.js
@@ -1,23 +1,28 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
 
-const path = require('path');
-const start = require('./index');
-const pessimist = require('pessimist')
-  .usage('Fetch and save the contents of an HLS playlist locally.\nUsage: $0 ')
-  .alias('i', 'input')
-  .demand('i')
-  .describe('i', 'uri to m3u8 (required)')
-  .alias('o', 'output')
-  .default('o', './hls-fetcher')
-  .describe('o', "output path (default:'./hls-fetcher')")
-  .alias('c', 'concurrency')
-  .default('c', Infinity)
-  .describe('c', 'number of simultaneous fetches (default: Infinity)')
-  .alias('d', 'decrypt')
-  .default('d', false)
-  .describe('d', 'decrypt and remove enryption from manifest (default: false)')
-  .argv;
+const path = require("path");
+const start = require("./index");
+const pessimist = require("pessimist")
+  .usage("Fetch and save the contents of an HLS playlist locally.\nUsage: $0 ")
+  .alias("i", "input")
+  .demand("i")
+  .describe("i", "uri to m3u8 (required)")
+  .alias("o", "output")
+  .default("o", "./hls-fetcher")
+  .describe("o", "output path (default:'./hls-fetcher')")
+  .alias("c", "concurrency")
+  .default("c", Infinity)
+  .describe("c", "number of simultaneous fetches (default: Infinity)")
+  .alias("d", "decrypt")
+  .default("d", false)
+  .describe("d", "decrypt and remove enryption from manifest (default: false)")
+  .alias("abs", "keepAbsSegmentPaths")
+  .default("abs", false)
+  .describe(
+    "abs",
+    "convert the segment URIs to absolute paths (inc protocol & hostname) (default: false)"
+  ).argv;
 
 // Make output path
 const output = path.resolve(pessimist.o);
@@ -26,15 +31,18 @@ const options = {
   input: pessimist.i,
   output,
   concurrency: pessimist.c,
-  decrypt: pessimist.d
+  decrypt: pessimist.d,
+  keepAbsSegmentPaths: pessimist.a,
 };
 
-start(options).then(function() {
-  const timeTaken = ((Date.now() - startTime) / 1000).toFixed(2);
+start(options)
+  .then(function () {
+    const timeTaken = ((Date.now() - startTime) / 1000).toFixed(2);
 
-  console.log('Operation completed successfully in', timeTaken, 'seconds.');
-  process.exit(0);
-}).catch(function(error) {
-  console.error('ERROR', error);
-  process.exit(1);
-});
+    console.log("Operation completed successfully in", timeTaken, "seconds.");
+    process.exit(0);
+  })
+  .catch(function (error) {
+    console.error("ERROR", error);
+    process.exit(1);
+  });
--- src/write-data.js
+++ src/write-data.js
@@ -1,20 +1,22 @@
 /* eslint-disable no-console */
-const Promise = require('bluebird');
-const mkdirp = require('mkdirp');
-const request = require('requestretry');
-const fs = Promise.promisifyAll(require('fs'));
-const AesDecrypter = require('aes-decrypter').Decrypter;
-const path = require('path');
+const Promise = require("bluebird");
+const mkdirp = require("mkdirp");
+const request = require("requestretry");
+const fs = Promise.promisifyAll(require("fs"));
+const AesDecrypter = require("aes-decrypter").Decrypter;
+const path = require("path");
 
-const writeFile = function(file, content) {
-  return mkdirp(path.dirname(file)).then(function() {
-    return fs.writeFileAsync(file, content);
-  }).then(function() {
-    console.log('Finished: ' + path.relative('.', file));
-  });
+const writeFile = function (file, content) {
+  return mkdirp(path.dirname(file))
+    .then(function () {
+      return fs.writeFileAsync(file, content);
+    })
+    .then(function () {
+      console.log("Finished: " + path.relative(".", file));
+    });
 };
 
-const requestFile = function(uri) {
+const requestFile = function (uri) {
   const options = {
     uri,
     // 60 seconds timeout
@@ -22,11 +24,11 @@ const requestFile = function(uri) {
     // treat all responses as a buffer
     encoding: null,
     // retry 1s after on failure
-    retryDelay: 1000
+    retryDelay: 1000,
   };
 
-  return new Promise(function(resolve, reject) {
-    request(options, function(err, response, body) {
+  return new Promise(function (resolve, reject) {
+    request(options, function (err, response, body) {
       if (err) {
         return reject(err);
       }
@@ -35,44 +37,55 @@ const requestFile = function(uri) {
   });
 };
 
-const toUint8Array = function(nodeBuffer) {
-  return new Uint8Array(nodeBuffer.buffer, nodeBuffer.byteOffset, nodeBuffer.byteLength / Uint8Array.BYTES_PER_ELEMENT);
+const toUint8Array = function (nodeBuffer) {
+  return new Uint8Array(
+    nodeBuffer.buffer,
+    nodeBuffer.byteOffset,
+    nodeBuffer.byteLength / Uint8Array.BYTES_PER_ELEMENT
+  );
 };
 
-const decryptFile = function(content, encryption) {
-  return new Promise(function(resolve, reject) {
+const decryptFile = function (content, encryption) {
+  return new Promise(function (resolve, reject) {
     /* eslint-disable no-new */
     // this is how you use it, its kind of bad but :shrug:
-    new AesDecrypter(toUint8Array(content), encryption.bytes, encryption.iv, function(err, bytes) {
-      if (err) {
-        return reject(err);
+    new AesDecrypter(
+      toUint8Array(content),
+      encryption.bytes,
+      encryption.iv,
+      function (err, bytes) {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(Buffer.from(bytes));
       }
-      return resolve(Buffer.from(bytes));
-    });
+    );
     /* eslint-enable no-new */
   });
 };
 
-const WriteData = function(decrypt, concurrency, resources) {
+const WriteData = function (decrypt, concurrency, manifestOnly, resources) {
   const inProgress = [];
   const operations = [];
 
-  resources.forEach(function(r) {
+  resources.forEach(function (r) {
     if (r.content) {
-      operations.push(function() {
+      operations.push(function () {
         return writeFile(r.file, r.content);
       });
     } else if (r.uri && r.key && decrypt) {
-      operations.push(function() {
-        return requestFile(r.uri).then(function(content) {
-          return decryptFile(content, r.key);
-        }).then(function(content) {
-          return writeFile(r.file, content);
-        });
+      operations.push(function () {
+        return requestFile(r.uri)
+          .then(function (content) {
+            return decryptFile(content, r.key);
+          })
+          .then(function (content) {
+            return writeFile(r.file, content);
+          });
       });
     } else if (r.uri && inProgress.indexOf(r.uri) === -1) {
-      operations.push(function() {
-        return requestFile(r.uri).then(function(content) {
+      operations.push(function () {
+        return requestFile(r.uri).then(function (content) {
           return writeFile(r.file, content);
         });
       });
@@ -80,10 +93,14 @@ const WriteData = function(decrypt, concurrency, resources) {
     }
   });
 
-  return Promise.map(operations, function(o) {
-    return Promise.join(o());
-  }, {concurrency}).all(function(o) {
-    console.log('DONE!');
+  return Promise.map(
+    operations,
+    function (o) {
+      return Promise.join(o());
+    },
+    { concurrency }
+  ).all(function (o) {
+    console.log("DONE!");
     return Promise.resolve();
   });
 };
